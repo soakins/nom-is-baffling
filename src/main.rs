f4d3dd2b02c0016f469e70c66a49631d6cf5f521
soakins <68757318+soakins@users.noreply.github.com>
2021-02-06 23:59:13 +0000

    Third example added
    This example uses 'pair' to combine two simpler tag parsers.

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,24 @@ fn main() {
     /*
      * Example two
      */
-     println!("{}", "Example Two");
-     let input_data = String::from("faulty input");
-     let two_dashes = "--";
-     let x: nom::IResult<&str, &str, nom::error::Error<&str>> = nom::bytes::complete::tag_no_case(two_dashes)(&input_data);
-     println!("{}", "This second example won't work, because I've fed it data that doesn't match the tag.");
-     println!("{:?}", x);
+    println!("{}", "Example Two");
+    let input_data = String::from("faulty input");
+    let two_dashes = "--";
+    let x: nom::IResult<&str, &str, nom::error::Error<&str>> = nom::bytes::complete::tag_no_case(two_dashes)(&input_data);
+    println!("{}", "This second example won't work, because I've fed it data that doesn't match the tag.");
+    println!("{:?}", x);
+
+
+    /*
+     * Example three
+     */
+    println!("{}", "Example Three");
+    let input_data = String::from("--hat");
+    let mut x = nom::sequence::pair(nom::bytes::complete::tag_no_case(two_dashes), nom::bytes::complete::tag_no_case("hat"));
+    let y: nom::IResult<&str, (&str, &str), nom::error::Error<&str>> = x(&input_data);
+    println!("{}", "This example uses two tag parsers, combined in a 'pair' parser. This time the pair is assigned to a variable (x), just for fun, and the variable is used to call the function to return the iResult into y. ");
+    println!("{}", "Noting the two output types, one for each parser in the pair, makes me realise that a parser could return anything, which might be useful to output a 'translation' of something matched. Maybe an enum variant instead of a str that will need to be further compared to 'read' what it says.");
+    println!("{}", "Note also that the variable x that holds the parser function needs to be declared mutable. I don't really know why.");
+    println!("{:?}", y);
 
 }
